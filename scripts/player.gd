d97da365d0c1a4ad56e72be111bdf9fd33fb89d4
fenix-bear <benttonp@protonmail.com>
2023-03-10 16:30:41 -0500

    pain

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,14 +8,21 @@ var maxSpeed = 350
 func respawn():
 	pass
 
+var processing = false
+
 func _process(delta):
-	var new_pos = position
-	if Input.is_action_pressed("moveUp"):
-		new_pos.y -= 1
-	if Input.is_action_pressed("moveDown"):
-		new_pos.y += 1
-	if Input.is_action_pressed("moveLeft"):
-		new_pos.x -= 1
-	if Input.is_action_pressed("moveRight"):
-		new_pos.x += 1
-	position = new_pos
+	if !processing:
+		processing = true
+		var new_pos = position
+		if Input.is_action_pressed("moveUp"):
+			new_pos.y -= 5
+		if Input.is_action_pressed("moveDown"):
+			new_pos.y += 5
+		if Input.is_action_pressed("moveLeft"):
+			new_pos.x -= 5
+		if Input.is_action_pressed("moveRight"):
+			new_pos.x += 5
+		if position != new_pos:
+			position = new_pos
+			print(str(new_pos) + str(int(Input.is_action_pressed("moveUp"))) + str(int(Input.is_action_pressed("moveDown"))) + str(int(Input.is_action_pressed("moveLeft"))) + str(int(Input.is_action_pressed("moveRight"))))
+		processing = false
