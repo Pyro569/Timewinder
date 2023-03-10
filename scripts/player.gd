e7aac1b7ba1a0d2f4dfdab899fb3deb081f87701
Pyro <doggolover98@outlook.com>
2023-03-10 16:53:43 -0500

    Animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,8 +5,13 @@ extends CharacterBody2D
 
 func get_input():
 	var input_direction = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
+	if velocity.x < 0:
+		$CollisionShape2D/AnimatedSprite2D.flip_h = true
+	elif velocity.x > 0:
+		$CollisionShape2D/AnimatedSprite2D.flip_h = false
 	velocity = input_direction * speed
 
 func _physics_process(delta):
 	get_input()
+	$CollisionShape2D/AnimatedSprite2D.play()
 	move_and_slide()
