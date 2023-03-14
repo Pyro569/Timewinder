d5dfa86130243ba333bc4bcb12cbca5638a9f862
fenix-bear <benttonp@protonmail.com>
2023-03-13 23:27:56 -0400

    fixed player start

--- scripts/tilelogic.gd
+++ scripts/tilelogic.gd
@@ -1,15 +1,13 @@
 extends TileMap
 
-var cell_size = 64
+@export var cell_size = 64
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var camerapos = get_parent().get_parent().get_node("Camera/Camera2D").position
-	print(camerapos)
-	for cell in get_used_cells_by_id(0, 7):
-		print(cell)
-		get_parent().get_parent().get_node("Camera/Camera2D").position = Vector2i((cell.x) * cell_size, (cell.y + 2.25) * cell_size)
-	print(get_parent().get_parent().get_node("Camera/Camera2D").position)
+	for cell in get_used_cells_by_id(0, 7): # future cam
+		get_node("/root/Node2D/Camera/Camera2D").position = Vector2i((cell.x) * cell_size, (cell.y + 2.25) * cell_size)
+	for cell in get_used_cells_by_id(0, 5): # player start
+		get_node("/root/Node2D/Player/CharacterBody2D").position = Vector2i((cell.x) * cell_size, (cell.y + 2.25) * cell_size)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
