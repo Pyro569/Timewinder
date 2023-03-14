cdca7354da553a2560ffe9aca6284296b1830a9f
fenix-bear <benttonp@protonmail.com>
2023-03-14 00:33:43 -0400

    fix tilemap alignment

--- scripts/tilelogic.gd
+++ scripts/tilelogic.gd
@@ -5,9 +5,9 @@ extends TileMap
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for cell in get_used_cells_by_id(0, 7): # future cam
-		get_node("/root/Node2D/Camera/Camera2D").position = Vector2i((cell.x) * cell_size, (cell.y + 2.25) * cell_size)
+		get_node("/root/Node2D/Camera/Camera2D").position = (cell + Vector2i(1, 1)) * cell_size
 	for cell in get_used_cells_by_id(0, 5): # player start
-		get_node("/root/Node2D/Player/CharacterBody2D").position = Vector2i((cell.x) * cell_size, (cell.y + 2.25) * cell_size)
+		get_node("/root/Node2D/Player/CharacterBody2D").position = (cell + Vector2i(1, 1)) * cell_size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
