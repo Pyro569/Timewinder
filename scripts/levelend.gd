1f9a28b26900c795e37cd17ffab9a449dd903758
Pyro <doggolover98@outlook.com>
2023-03-16 16:56:37 -0400

    Fixed wacky script

--- scripts/levelend.gd
+++ scripts/levelend.gd
@@ -4,7 +4,3 @@ func _on_body_entered(body):
 	if body.is_class("CharacterBody2D"):
 		print("Collided")
 		get_tree().change_scene_to_file("res://scenes/level2.tscn")
-
-
-func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	pass # Replace with function body.
