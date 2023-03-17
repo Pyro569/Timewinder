4a36beb704f1eb29074e1abf4cc1455e138d8714
Pyro <doggolover98@outlook.com>
2023-03-17 16:56:31 -0400

    Fixed tyler's blunder

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,17 +2,12 @@ class_name Player
 extends CharacterBody2D
 
 @export var speed = 200
-var i = 0
 func get_input():
 	var input_direction = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
 	if velocity.x < 0:
 		$CollisionShape2D/AnimatedSprite2D.flip_h = true
 	elif velocity.x > 0:
 		$CollisionShape2D/AnimatedSprite2D.flip_h = false
-	elif velocity.x == 0 && i == 0:
-		var duplicated_node = self.duplicate()
-		self.add_child(duplicated_node)
-		i += 1
 	velocity = input_direction * speed
 
 func _physics_process(delta):
