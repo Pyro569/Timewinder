43585624dfcc12246782417a01f3a2396b4c60be
fenix-bear <benttonp@protonmail.com>
2023-03-17 09:17:43 -0400

    non-hardcoded level transition

--- scripts/levelend.gd
+++ scripts/levelend.gd
@@ -2,4 +2,6 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_class("CharacterBody2D"):
-		get_tree().change_scene_to_file("res://scenes/level2.tscn")
+		var scene_name = get_tree().current_scene.get_name()
+		print("res://scenes/level" + str(int(scene_name.substr(len(str(scene_name)) - 1)) + 1) + ".tscn")
+		get_tree().change_scene_to_file("res://scenes/level" + str(int(scene_name.substr(len(str(scene_name)) - 1)) + 1) + ".tscn")
