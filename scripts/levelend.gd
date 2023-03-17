5a88e48f4eccf9404a513acfa41047d5bed869da
fenix-bear <benttonp@protonmail.com>
2023-03-17 09:45:18 -0400

    new objects

--- scripts/levelend.gd
+++ scripts/levelend.gd
@@ -3,5 +3,4 @@ extends Area2D
 func _on_body_entered(body):
 	if body.is_class("CharacterBody2D"):
 		var scene_name = get_tree().current_scene.get_name()
-		print("res://scenes/level" + str(int(scene_name.substr(len(str(scene_name)) - 1)) + 1) + ".tscn")
 		get_tree().change_scene_to_file("res://scenes/level" + str(int(scene_name.substr(len(str(scene_name)) - 1)) + 1) + ".tscn")
