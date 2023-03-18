a6b28610a8b5ab30253bd8009a89301ced0667d5
Pyro <doggolover98@outlook.com>
2023-03-18 15:14:57 -0400

    Added bad temporary title screen

---
+++ scripts/titlescreenbutton.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+	
+func _physics_process(delta):
+	if Input.is_action_pressed("space"):
+		get_tree().change_scene_to_file("res://scenes/level1.tscn")
