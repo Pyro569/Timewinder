df04254348c56c40cd16f81309658b0656a47aa0
fenix-bear <benttonp@protonmail.com>
2023-03-18 17:20:17 -0400

    level transition supports multidigit level nums

--- scripts/levelend.gd
+++ scripts/levelend.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_class("CharacterBody2D"):
-		var scene_name = str(get_tree().current_scene.scene_file_path)
-		print("res://scenes/level" + str(int(scene_name.substr(len(scene_name) - 6, len(scene_name) - 5)) + 1) + ".tscn")
-		get_tree().change_scene_to_file("res://scenes/level" + str(int(scene_name.substr(len(scene_name) - 6, len(scene_name) - 5)) + 1) + ".tscn")
+		var scene_name = "res://scenes/level10.tscn"
+		var new_scene = "res://scenes/level" + str(int(scene_name.substr(5, len(scene_name) - 5)) + 1) + ".tscn"
+		print(new_scene)
+		get_tree().change_scene_to_file(new_scene)
