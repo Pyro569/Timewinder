6b79681d77fde9b5249ffcea55e11572147cb0de
fenix-bear <benttonp@protonmail.com>
2023-03-18 19:22:11 -0400

    added duplication back

--- scripts/tilelogic.gd
+++ scripts/tilelogic.gd
@@ -13,11 +13,18 @@ var noTravel = preload("res://assets/sounds/effects/noTravel.wav")
 var playnotravelsound = preload("res://assets/sounds/effects/noTravel.wav")
 
 var id_to_node = {
-	7: "Camera/Camera2D",
 	5: "Player/CharacterBody2D",
 	4: "Level/MagnetLevelEnd",
-	0: "Level/Box"
+	0: "Level/Box",
+	3: "Level/Button"
 }
+
+var MAX_SIGNAL_LENGTH = 64
+var activator_ids = [3]
+var activatee_ids = [4]
+var activators = {}
+var activatees = {}
+var signals = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,9 +32,17 @@ func _ready():
 	# setup tilemap
 	for id in id_to_node:
 		for cell in get_used_cells_by_id(0, id):
-			get_node("/root/Node2D/" + id_to_node[id]).global_position = cell * cell_size
+			var parent_node = get_node("/root/Node2D/" + id_to_node[id])
+			var new_node = parent_node.duplicate()
+			parent_node.add_child(new_node)
+			new_node.global_position = cell * cell_size
+			if activator_ids.has(id):
+				activators[cell] = new_node
+			if activatee_ids.has(id):
+				activatees[cell] = new_node
 	# edge-cases
 	for cell in get_used_cells_by_id(0, 7): # future cam
+		get_node("/root/Node2D/Camera/Camera2D").global_position = cell * cell_size
 		future_cam = cell * cell_size
 	for cell in get_used_cells_by_id(0, 6): # past cam
 		past_cam = cell * cell_size
@@ -35,7 +50,15 @@ func _ready():
 		var player_size = Vector2i(get_node("/root/Node2D/Player/CharacterBody2D/CollisionShape2D").get_shape().get_rect().size)
 		get_node("/root/Node2D/Player/CharacterBody2D").position += Vector2((cell_size - player_size) / 2)
 	# signals
-	
+	for activator_id in activator_ids:
+		for cell in get_used_cells_by_id(0, activator_id):
+			var signal_end = cell - Vector2i(MAX_SIGNAL_LENGTH, 0)
+			var current_cell = cell
+			while signal_end.x < current_cell.x:
+				current_cell -= Vector2i(1, 0)
+				if activatees.has(current_cell):
+					signals[activators[cell]] = activatees[cell]
+			
 	
 		
 	add_child(timer)
