dd9a932fd1c102c9115b7acbd70e2f218239b872
KobiSteve07 <109444953+KobiSteve07@users.noreply.github.com>
2023-03-18 22:00:01 -0400

    skippable intro (esc)

--- scenes/VideoStreamPlayer.gd
+++ scenes/VideoStreamPlayer.gd
@@ -8,5 +8,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(!(is_playing())):
+	if(!(is_playing()) || Input.is_action_pressed("escape")):
 		get_tree().change_scene_to_file("res://scenes/titlescreen.tscn")
+	
--- scripts/camera.gd
+++ scripts/camera.gd
@@ -4,9 +4,6 @@ var intended_x = 1152.0
 var intended_y = 648.0
 
 func _process(delta):
-	var player = get_node("/root/Node2D/Player/CharacterBody2D")
-	var velocity = Vector2.ZERO
-	var iterations = 1
 	var viewport_size = get_viewport_rect().size
 	var intended_aspect_ratio = intended_x / intended_y
 	var viewport_aspect_ratio = viewport_size.x / viewport_size.y
