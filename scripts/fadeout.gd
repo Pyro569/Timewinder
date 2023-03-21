f04de53350592cce9fa3a603e49b4ae42d1727e5
Pyro <doggolover98@outlook.com>
2023-03-21 09:08:28 -0400

    Added trashy travel counter in levels (fix up later)

--- scripts/fadeout.gd
+++ scripts/fadeout.gd
@@ -1,12 +1,11 @@
 extends ColorRect
 
-func fadeout():
+var screensize
+
+func _ready():
 	var fadeoutRect = get_node("/root/Node2D/Effects/Fadeout")
 	var animPlayer = get_node("/root/Node2D/Effects/AnimationPlayer")
-	var screensize
 	screensize = get_viewport_rect().size
 	fadeoutRect.set_position(-(screensize/2))
 	fadeoutRect.set_size(screensize*1000000)
-	print("Playing fade animation")
-	animPlayer.play("fade_to_black")
-	print("Played fade animation")
+	animPlayer.play_backwards("fade_to_black")
--- scripts/tilelogic.gd
+++ scripts/tilelogic.gd
@@ -9,9 +9,15 @@ var time_travelling = false
 var timer = Timer.new()
 var unixTime = 3
 
+var timeTravels = 0
+
 var noTravel = preload("res://assets/sounds/effects/noTravel.wav")
 var playnotravelsound = preload("res://assets/sounds/effects/noTravel.wav")
 var play_yes_travel_sounds = [preload("res://assets/sounds/effects/yesTravel1.wav")]
+
+var screensize
+var travelCounterNode
+var travelCounterNode2
 
 var pressed_time_travel_last_frame = false
 
@@ -69,6 +75,11 @@ func resolve_signal(activator: Vector2i, direction: int, past_arrows = []):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	travelCounterNode = get_node("/root/Node2D/Effects/TravelCounter")
+	travelCounterNode2 = get_node("/root/Node2D/Effects/TravelCounter2")
+	
+	travelCounterNode.text = "Travels: 0"
+	travelCounterNode2.text = "Travels: 0"
 	# setup tilemap
 	for id in id_to_node:
 		for cell in get_used_cells_by_id(0, id):
@@ -100,8 +111,6 @@ func _ready():
 				Globals.signals[activators[cell]] = null
 					
 	#print(Globals.signals)
-			
-	
 		
 	add_child(timer)
 	timer.set_wait_time(1.0)
@@ -127,11 +136,16 @@ func _process(_delta):
 				get_node("/root/Node2D/Camera/Camera2D").position = past_cam
 				get_node("/root/Node2D/Player/CharacterBody2D").position += Vector2(past_cam - future_cam)
 				unixTime = 0
+				timeTravels = timeTravels + 1
+				travelCounterNode.text = "Travels: " + str(timeTravels)
+				travelCounterNode2.text = "Travels: " + str(timeTravels)
 			else:
 				future = true
 				get_node("/root/Node2D/Camera/Camera2D").position = future_cam
 				get_node("/root/Node2D/Player/CharacterBody2D").position += Vector2(future_cam - past_cam)
-				unixTime = 0
+				timeTravels = timeTravels + 1
+				travelCounterNode.text = "Travels: " + str(timeTravels)
+				travelCounterNode2.text = "Travels: " + str(timeTravels)
 	elif unixTime < 2 and Input.is_action_pressed("timeTravel") and !pressed_time_travel_last_frame:
 		# This executes the sound as soon and only once when the designetated key for time trabeling is pressed
 		get_node("/root/Node2D/Camera/Camera2D/AudioStreamPlayer").stream = playnotravelsound
