d0b72771e4f027d320e70fb2bcf082e58254b9a7
fenix-bear <benttonp@protonmail.com>
2023-03-24 03:48:55 -0400

    e

--- scripts/duplicant/activatees/levelend.gd
+++ scripts/duplicant/activatees/levelend.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 func _on_body_entered(body):
 	print(body.get_name())
-	if body.get_name() == "RigidBody2D" and get_parent().activators > 0:
+	if body.get_parent().get_name() == "Player" and get_parent().activators > 0:
 		var scene_name = str(get_tree().current_scene.scene_file_path)
 		var new_scene = "res://scenes/level" + str(int(scene_name.substr(5, len(scene_name) - 5)) + 1) + ".tscn"
 		get_tree().change_scene_to_file(new_scene)
