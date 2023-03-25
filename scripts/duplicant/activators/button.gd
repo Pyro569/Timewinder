f9bee81844e3745debbac1b6fd00e0dd5de10ab7
Pyro <doggolover98@outlook.com>
2023-03-24 22:08:16 -0400

    Made button ago up and down and fixed robit

--- scripts/duplicant/activators/button.gd
+++ scripts/duplicant/activators/button.gd
@@ -3,8 +3,10 @@ extends Area2D
 func _on_body_entered(body):
 	print("ENTER: ", body.get_name())
 	print(Globals.signals)
+	$AnimatedSprite2D.frame = 3
 	get_parent().activate()
 
 func _on_body_exited(body):
 	print("EXIT: ", body.get_name())
+	$AnimatedSprite2D.frame = 0
 	get_parent().deactivate()
