8f210eb293a5ea907cb5c67260a9c87452d33faf
hidude562 <poopybutface8@gmail.com>
2023-03-26 12:17:20 -0400

    fix

--- scripts/Menu/nextLevelButton.gd
+++ scripts/Menu/nextLevelButton.gd
@@ -1,10 +1,10 @@
 extends Button
 
 func nextLevel():
+	Globals.levelPlayerIsOn += 1
 	get_tree().change_scene_to_file("res://scenes/level" + str(Globals.levelPlayerIsOn + 1) + ".tscn")
 
 func _on_button_down():
-	Globals.levelPlayerIsOn += 1
 	nextLevel()
 	pass # Replace with function body.
 
