93bfab2d12d1c006dba641248464a31bc1dccd59
Pyro <doggolover98@outlook.com>
2023-03-26 10:21:27 -0400

    A few fixes

--- scripts/duplicant/activators/button.gd
+++ scripts/duplicant/activators/button.gd
@@ -5,8 +5,3 @@ func _on_body_entered(body):
 	print(Globals.signals)
 	$AnimatedSprite2D.frame = 3
 	get_parent().activate()
-
-func _on_body_exited(body):
-	print("EXIT: ", body.get_name())
-	$AnimatedSprite2D.frame = 0
-	get_parent().deactivate()
