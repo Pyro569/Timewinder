0bed551697ecad13c39952e15481d85113329151
hidude562 <poopybutface8@gmail.com>
2023-03-26 11:58:35 -0400

    make menu better

--- scripts/Menu/menuButtonLevelSelector.gd
+++ scripts/Menu/menuButtonLevelSelector.gd
@@ -3,4 +3,5 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _on_button_down():
+	Globals.levelPlayerIsOn = 0
 	get_tree().change_scene_to_file("res://scenes/titlescreen.tscn")
