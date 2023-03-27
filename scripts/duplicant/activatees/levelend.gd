11160f7e2a54b86b0a77f851e8e431b2860963c1
Nathan M <82677882+hidude562@users.noreply.github.com>
2023-03-27 16:15:17 -0400

    extra decimal places

--- scripts/duplicant/activatees/levelend.gd
+++ scripts/duplicant/activatees/levelend.gd
@@ -28,6 +28,6 @@ func _process(_delta):
 		done = false
 		print(get_node("/root/Node2D/Level/TileMap").get("bestTime"))
 		# Updates best times to the globals list with this
-		Globals.thisLevels.endLevelCheck(get_node("/root/Node2D/Level/TileMap").get("timeTravels"), (Time.get_ticks_msec() - get_node("/root/Node2D/Level/TileMap").get("gameStartTime")) / 1000)
+		Globals.thisLevels.endLevelCheck(get_node("/root/Node2D/Level/TileMap").get("timeTravels"), (Time.get_ticks_msec() - get_node("/root/Node2D/Level/TileMap").get("gameStartTime")) / 1000.0)
 		var new_scene = "res://scenes/levelWin.tscn"
 		get_tree().change_scene_to_file(new_scene)
