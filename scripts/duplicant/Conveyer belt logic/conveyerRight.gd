3e6ad6234fadf9382b8c980a64e7a4bd6a88a43b
Nathan M <poopybutface8@gmail.com>
2023-03-27 11:23:12 -0400

    make level 8 better
    finally got the convayer fully working

--- scripts/duplicant/Conveyer belt logic/conveyerRight.gd
+++ scripts/duplicant/Conveyer belt logic/conveyerRight.gd
@@ -25,7 +25,8 @@ func modifyBodySpeed(body, movement):
 func _process(_delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
-		modifyBodySpeed(body, Vector2(1, 0))
+		if(body.get("constant_force").x == 0 and body.get("constant_force").y == 0):
+			modifyBodySpeed(body, Vector2(1, 0))
 
 func _on_body_exited(body):
 	modifyBodySpeed(body, Vector2(0, 0))
