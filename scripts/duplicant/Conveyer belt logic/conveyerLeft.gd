dc330d163145cad3b3227d64d2a51a3e4ff68dd9
Nathan M <poopybutface8@gmail.com>
2023-03-27 09:46:10 -0400

    Fix belt

--- scripts/duplicant/Conveyer belt logic/conveyerLeft.gd
+++ scripts/duplicant/Conveyer belt logic/conveyerLeft.gd
@@ -1,5 +1,22 @@
 extends Area2D
 
+var init_pos: Vector2i
+var prev_pos: Vector2
+var travelvects = []
+
+func future():
+	print("yes: ", name)
+	for vect in travelvects:
+		position += vect
+	position -= Globals.cam_delta
+	
+func past():
+	print("yes: ", name)
+	for vect in travelvects:
+		position -= vect
+	position += Globals.cam_delta
+
+
 var speed = 35
 
 func modifyBodySpeed(body, movement):
