e48bc810b1f750be930f668b1825d8006c1f10b8
fenix-bear <benttonp@protonmail.com>
2023-03-27 22:30:46 -0400

    magnet activated sprites implemented

--- scripts/switchFuturePastFrame.gd
+++ scripts/switchFuturePastFrame.gd
@@ -2,7 +2,23 @@ extends AnimatedSprite2D
 
 # Set to second frame if it is future
 func _process(delta):
+	var actives = get_parent().get_parent().get("activators")
 	var obj = get_node_or_null("/root/Node2D/Level/TileMap")
 	if obj != null:
-		set_frame(obj.get("future"))
+		if obj.get("future"):
+			if actives != null:
+				if actives > 0:
+					set_frame(3)
+				else:
+					set_frame(1)
+			else:
+				set_frame(1)
+		else:
+			if actives != null:
+				if actives > 0:
+					set_frame(2)
+				else:
+					set_frame(0)
+			else:
+				set_frame(0)
 	pass
